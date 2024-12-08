cac742c8bcd591f48d237170008f3a7c5a077315
Khaled <hy3113194@gmail.com>
2024-12-08 16:42:27 +0200

    adding bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:touf_w_shouf/core/di/service_locator.dart';
+import 'package:touf_w_shouf/core/helpers/simple_bloc_observer.dart';
 import 'package:touf_w_shouf/core/helpers/system_ui_config.dart';
 import 'package:touf_w_shouf/core/routing/app_router.dart';
 import 'package:touf_w_shouf/core/shared/shared_pref.dart';
@@ -10,6 +12,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPref.init();
   await ScreenUtil.ensureScreenSize();
+  Bloc.observer = SimpleBlocObserver();
   setupServiceLocator();
   SystemUIConfig.configure();
   runApp(
